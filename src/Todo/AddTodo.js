850955bfc78b7d9740ccf22dc21e6322a379de3e
macbook <andr_lush@ukr.net>
2022-05-20 19:39:51 +0300

    first commit

---
+++ src/Todo/AddTodo.js
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import PropTypes from 'prop-types'
+
+function useInputValue(defaultValue=''){
+    const [value, setValue] = useState(defaultValue)
+
+    return {
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+
+}
+
+function AddTodo({onCreate}) {
+    const input = useInputValue('')
+
+    // Отменяем дефолтное поведение чтобы стр не ребуталась
+    function submitHandler(event) {
+        event.preventDefault()
+    // Удаляем лишние пробелы
+        if (input.value().trim()) {
+            onCreate(input.value())
+            input.clear()
+             // Очистить данное значение
+
+    }
+}
+
+    return (
+        <form style={{marginBottom: '1rem'}} onSubmit={submitHandler}>
+            <input {...input.bind}/>
+            <button type="submit">Add todo</button>
+
+        </form>
+    )
+
+}
+
+AddTodo.propTypes = {
+    onCreate: PropTypes.func.isRequired
+}
+
+export default AddTodo
---
+++ src/context.js
@@ -0,0 +1,6 @@
+// Фича для передачи определенного св-ва, избегаю промежут. этапов(ф-ии от дочернего эл-та к родительскому). 
+import React from "react";
+
+const Context = React.createContext()
+
+export default Context
